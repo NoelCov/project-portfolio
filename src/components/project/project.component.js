6f8a25a137f9b6d3811e47c79964a8a308d65612
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-07-05 02:30:46 -0700

    Fixed some issues with the sizes of the image

--- src/components/project/project.component.js
+++ src/components/project/project.component.js
@@ -17,7 +17,7 @@ const Project = projectData => {
   return (
     <div className="project">
       <div className="image-container">
-      <img src={img} className="image" alt="project" />
+        <img src={img} className="image" alt="project" />
       </div>
       <h2 className="project-title">{title}</h2>
       <span className="text">{text}</span>
